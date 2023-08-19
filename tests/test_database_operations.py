b2f39a7317cd1ad0c24ac3df033ea0d9ee9579cd
Walid Mujahid <walidmujahid@gmail.com>
2023-08-19 08:29:02 -0400

    refactor(tests): add descriptive assertion messages, ensure rollbacks, and handle error scenarios

--- tests/test_database_operations.py
+++ tests/test_database_operations.py
@@ -51,108 +51,146 @@ def db_session(request):
 
 def test_create_video_record(db_session):
     # Test creating a video record in the database
+    try:
+        # Sample video information
+        video_source = 'youtube'
+        video_url = 'https://youtu.be/PXTsVwd_j5A'
+        video_id = 'PXTsVwd_j5A'
+        title = 'THE JAMIL AL-AMIN CONSPIRACY'
+        description = 'The Jamil Al-Amin (H. Rap Brown) Conspiracy. A true leader who walked with Martin Luther King, propagated the ideas of Malcolm X and became the Minister of Defense of the Black Panther Party is still alive and jailed under suspicious circumstances. Free Jamil Al Amin!'
+        video_url = 'https://www.youtube.com/watch?v=test_video_id'
+        thumbnail_url = 'https://example.com/sample_thumbnail.jpg'
+        video_info = {
+            'video_source': 'youtube',
+            'id': video_id,
+            'title': title,
+            'description': description,
+            'thumbnail': thumbnail_url,
+        }
 
-    # Sample video information
-    video_info = {
-        'video_source': 'youtube',
-        'id': 'PXTsVwd_j5A',
-        'title': 'THE JAMIL AL-AMIN CONSPIRACY',
-        'description': 'The Jamil Al-Amin (H. Rap Brown) Conspiracy. A true leader who walked with Martin Luther King, propagated the ideas of Malcolm X and became the Minister of Defense of the Black Panther Party is still alive and jailed under suspicious circumstances. Free Jamil Al Amin!',
-        'thumbnail': 'https://example.com/sample_thumbnail.jpg',
-    }
+        # Perform the operation
+        create_video_record(db_session, video_info, video_url)
 
-    # Perform the operation
-    create_video_record(db_session, video_info, 'https://youtu.be/PXTsVwd_j5A')
-
-    # Check if the video record is created
-    video = db_session.query(Video).filter(Video.video_id == 'PXTsVwd_j5A').first()
-    assert video is not None
-    assert video.video_source == 'youtube'
-    assert video.title == 'THE JAMIL AL-AMIN CONSPIRACY'
-    assert video.description == 'The Jamil Al-Amin (H. Rap Brown) Conspiracy. A true leader who walked with Martin Luther King, propagated the ideas of Malcolm X and became the Minister of Defense of the Black Panther Party is still alive and jailed under suspicious circumstances. Free Jamil Al Amin!'
-    assert video.video_url == 'https://youtu.be/PXTsVwd_j5A'
-    assert video.thumbnail_url == 'https://example.com/sample_thumbnail.jpg'
+        # Check if the video record is created
+        video = db_session.query(Video).filter(Video.video_id == video_id).first()
+        assert video is not None, "Video record not found"
+        assert video.video_source == video_source, f"Expected {video_source}, but got {video.video_source}"
+        assert video.title == title, f"Expected {title}, but got {video.title}"
+        assert video.description == description, f"Expected {description}, but got {video.description}"
+        assert video.video_url == video_url, f"Expected {video_url}, but got {video.video_url}"
+        assert video.thumbnail_url == thumbnail_url, f"Expected {thumbnail_url}, but got {video.thumbnail_url}"
+    except Exception as e:
+        raise e
+    finally:
+        db_session.rollback()
 
 def test_create_download_record(db_session):
     # Test creating a download record in the database
+    try:
+        # Sample video information
+        video_source = 'youtube'
+        video_id = 'PXTsVwd_j5A'
+        download_path = 'download_archive/PXTsVwd_j5A.mp4'
+        video_info = {
+            'video_source': video_source,
+            'id': video_id,
+            'download_path': download_path,
+        }
 
-    # Sample video information
-    video_info = {
-        'video_source': 'youtube',
-        'id': 'PXTsVwd_j5A',
-        'download_path': 'download_archive/PXTsVwd_j5A.mp4',
-    }
+        # Perform the operation
+        create_download_record(db_session, video_info)
 
-    # Perform the operation
-    create_download_record(db_session, video_info)
-
-    # Check if the download record is created
-    download = db_session.query(Download).filter(Download.video_id == 'PXTsVwd_j5A').first()
-    assert download is not None
-    assert download.video_source == 'youtube'
-    assert download.video_id == 'PXTsVwd_j5A'
-    assert download.download_path == 'download_archive/PXTsVwd_j5A.mp4'
+        # Check if the download record is created
+        download = db_session.query(Download).filter(Download.video_id == video_id).first()
+        assert download is not None, "Download record not found"
+        assert download.video_source == video_source, f"Expected {video_source}, but got {download.video_source}"
+        assert download.video_id == video_id, f"Expected {video_id}, but got {download.video_id}"
+        assert download.download_path == download_path, f"Expected {download_path}, but got {download.download_path}"
+    except Exception as e:
+        raise e
+    finally:
+        db_session.rollback()
 
 def test_create_lbry_upload_record(db_session):
     # Test creating an LBRYUpload record in the database
+    try:
+        # Sample video information
+        video_source = 'youtube'
+        video_id = 'PXTsVwd_j5A'
+        lbry_status = 0
+        lbry_url = None
+        video_info = {
+            'video_source': video_source,
+            'id': video_id,
+        }
 
-    # Sample video information
-    video_info = {
-        'video_source': 'youtube',
-        'id': 'PXTsVwd_j5A',
-    }
+        # Perform the operation
+        create_lbry_upload_record(db_session, video_info)
 
-    # Perform the operation
-    create_lbry_upload_record(db_session, video_info)
-
-    # Check if the LBRYUpload record is created
-    lbry_upload = db_session.query(LBRYUpload).filter(LBRYUpload.video_id == 'PXTsVwd_j5A').first()
-    assert lbry_upload is not None
-    assert lbry_upload.video_source == 'youtube'
-    assert lbry_upload.video_id == 'PXTsVwd_j5A'
-    assert lbry_upload.lbry_status == 0
-    assert lbry_upload.lbry_url is None
+        # Check if the LBRYUpload record is created
+        lbry_upload = db_session.query(LBRYUpload).filter(LBRYUpload.video_id == video_id).first()
+        assert lbry_upload is not None, "LBRYUpload record not found"
+        assert lbry_upload.video_source == video_source, f"Expected {video_source}, but got {lbry_upload.video_source}"
+        assert lbry_upload.video_id == video_id, f"Expected {video_id}, but got {lbry_upload.video_id}"
+        # on initial creation LBRYUpload records status should be 0
+        assert lbry_upload.lbry_status == lbry_status, f"Expected {lbry_status}, but got {lbry_uplaod.lbry_status}"
+        assert type(lbry_upload.lbry_status) is int, f"Expected {lbry_upload.lbry_status} to be of type class int"
+        # on initial creation LBRYUpload records lbry_url should be None
+        assert lbry_upload.lbry_url is lbry_url, f"Expected {lbry_upload.lbry_url} to be None"
+    except Exception as e:
+        raise e
+    finally:
+        db_session.rollback()
 
 def test_update_lbry_upload_record(db_session):
-    # Setup: Create an initial LBRYUpload record
-    video_info = {
-        'video_source': 'youtube',
-        'id': 'PXTsVwd_j5A',
-    }
-    create_lbry_upload_record(db_session, video_info)
+    try:
+        # Setup: Create an initial LBRYUpload record
+        video_info = {
+            'video_source': 'youtube',
+            'id': 'PXTsVwd_j5A',
+        }
+        create_lbry_upload_record(db_session, video_info)
 
-    # Test updating an LBRYUpload record in the database
-    video_id = 'PXTsVwd_j5A'
-    lbry_url = 'https://lbry.tv/@your_channel/PXTsVwd_j5A'
-    lbry_status = 1
+        # Test updating an LBRYUpload record in the database
+        video_id = 'PXTsVwd_j5A'
+        lbry_url = 'https://lbry.tv/@your_channel/PXTsVwd_j5A'
+        lbry_status = 1
 
-    # Perform the operation
-    update_lbry_upload_record(db_session, video_id, lbry_url, lbry_status)
+        # Perform the operation
+        update_lbry_upload_record(db_session, video_id, lbry_url, lbry_status)
 
-    # Check if the LBRYUpload record is updated
-    lbry_upload = db_session.query(LBRYUpload).filter(LBRYUpload.video_id == video_id).first()
+        # Check if the LBRYUpload record is updated
+        lbry_upload = db_session.query(LBRYUpload).filter(LBRYUpload.video_id == video_id).first()
 
-    # Assertions
-    assert lbry_upload is not None, "LBRYUpload record not found"
-    assert lbry_upload.video_id == video_id, f"Expected video_id {video_id}, but got {lbry_upload.video_id}"
-    assert lbry_upload.lbry_url == lbry_url, f"Expected lbry_url {lbry_url}, but got {lbry_upload.lbry_url}"
-    assert lbry_upload.lbry_status == lbry_status, f"Expected lbry_status {lbry_status}, but got {lbry_upload.lbry_status}"
+        # Assertions
+        assert lbry_upload is not None, "LBRYUpload record not found"
+        assert lbry_upload.video_id == video_id, f"Expected video_id {video_id}, but got {lbry_upload.video_id}"
+        assert lbry_upload.lbry_url == lbry_url, f"Expected lbry_url {lbry_url}, but got {lbry_upload.lbry_url}"
+        assert lbry_upload.lbry_status == lbry_status, f"Expected lbry_status {lbry_status}, but got {lbry_upload.lbry_status}"
+        assert lbry_upload.video_source == 'youtube', f"Expected video_source 'youtube', but got {lbry_upload.video_source}"
+        assert type(lbry_upload.lbry_status) is int, f"Expected {lbry_upload.lbry_status} to be of type class int"
 
-    # Additional Assertions
-    assert lbry_upload.video_source == 'youtube', f"Expected video_source 'youtube', but got {lbry_upload.video_source}"
-
-    # Teardown: Delete the LBRYUpload record
-    db_session.delete(lbry_upload)
-    db_session.commit()
+        # Teardown: Delete the LBRYUpload record
+        db_session.delete(lbry_upload)
+        db_session.commit()
+    except Exception as e:
+        raise e
+    finally:
+        db_session.rollback()
 
 # Error Scenarios
 def test_update_nonexistent_lbry_upload_record(db_session):
-    # Try updating a non-existent LBRYUpload record
-    video_id = 'NON_EXISTENT_ID'
-    lbry_url = 'https://lbry.tv/@your_channel/NON_EXISTENT_ID'
-    lbry_status = 1
+    try:
+        # Try updating a non-existent LBRYUpload record
+        video_id = 'NON_EXISTENT_ID'
+        lbry_url = 'https://lbry.tv/@your_channel/NON_EXISTENT_ID'
+        lbry_status = 1
 
-    # Expect an exception to be raised
-    with pytest.raises(sqlalchemy.exc.NoResultFound):
-        update_lbry_upload_record(db_session, video_id, lbry_url, lbry_status)
+        # Expect an exception to be raised
+        with pytest.raises(sqlalchemy.exc.NoResultFound):
+            update_lbry_upload_record(db_session, video_id, lbry_url, lbry_status)
+    except Exception as e:
+        raise e
+    finally:
+        db_session.rollback()
 
